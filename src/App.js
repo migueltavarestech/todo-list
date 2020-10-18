e34c2805195c951cf9ae0f87e2cc9cf645980d0c
Miguel <mike.lo.123@hotmail.com>
2020-10-18 18:37:31 +0100

    Save TodoList state to local machine

--- src/App.js
+++ src/App.js
@@ -11,25 +11,44 @@ function App() {
   const [status, setStatus] = useState('all');
   const [filteredTodos, setFilteredTodos] = useState([]);
 
+  // RUN ONCE when the app start
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
   // USE EFFECT
   useEffect(() => {
+    const filterHandler = () => {
+      switch(status){
+        case 'completed':
+          setFilteredTodos(todos.filter(todo => todo.completed === true));
+          break;
+        case 'uncompleted':
+          setFilteredTodos(todos.filter(todo => todo.completed === false));
+          break;
+        default:
+          setFilteredTodos(todos);
+          break;
+      };
+    };
     filterHandler();
+    saveLocalTodos();
   }, [todos, status]);
 
-  // Functions
-  const filterHandler = () => {
-    switch(status){
-      case 'completed':
-        setFilteredTodos(todos.filter(todo => todo.completed === true));
-        break;
-      case 'uncompleted':
-        setFilteredTodos(todos.filter(todo => todo.completed === false));
-        break;
-      default:
-        setFilteredTodos(todos);
-        break;
-    };
+  // Save to Local
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
   };
+
+  const getLocalTodos = () => {
+    if(localStorage.getItem('todos') === null){
+      localStorage.setItem('todos', JSON.stringify([]));
+    }else{
+      let todoLocal = JSON.parse(localStorage.getItem('todos'));
+      setTodos(todoLocal);
+    };
+  } 
+
 
   return (
     <div className="App">
