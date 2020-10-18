c8b9cb5495419245da6380656a22c56b9d6e1183
Miguel <mike.lo.123@hotmail.com>
2020-10-18 18:45:07 +0100

    Updates

---
+++ precache-manifest.dbecc5266952c2cfcf32fd7ae059d3df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e143e54163255dc21f93ce20a4f739",
+    "url": "/index.html"
+  },
+  {
+    "revision": "543bf173123a4e4ab4c3",
+    "url": "/static/css/main.92bd934a.chunk.css"
+  },
+  {
+    "revision": "7905087fcd2ec96111b1",
+    "url": "/static/js/2.3b70fed5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3b70fed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543bf173123a4e4ab4c3",
+    "url": "/static/js/main.825b0950.chunk.js"
+  },
+  {
+    "revision": "bca2f208509ed9d2e542",
+    "url": "/static/js/runtime-main.dbd73d5b.js"
+  }
+]);
